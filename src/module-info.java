57ef246e8775d65b207291c66712d254fcf3ba40
UrtziCM <urcrmedev@gmail.com>
2023-10-21 12:56:17 +0200

    UPDATE updated module-info.java

--- src/module-info.java
+++ src/module-info.java
@@ -5,4 +5,8 @@
  * 
  */
 module EjercicioO {
+	requires javafx.fxml;
+	requires javafx.graphics;
+	requires javafx.controls;
+	opens com.dein.productolabelwidget to javafx.base, javafx.controls,javafx.fxml;
 }
